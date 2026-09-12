7c555b7a5b285e5fcedc0b3e5c4c76001657e905
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-12 21:50:25 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,8 +3,8 @@ import fs from "fs";
 
 // Mega authentication credentials
 const auth = {
-    email: "dilshanashinsa793@gmail.com", 
-    password: "P3i2pc:we6JmpP_", 
+    email: "dilshanashinsa793@gmail.com", // your mega account login email
+    password: "P3i2pc:we6JmpP_", // your mega account login password
     userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -33,14 +33,7 @@ export const upload = (filePath, fileName) => {
                             reject(err);
                         } else {
                             storage.close();
-                            
-                            // 🛠️ මෙතනින් https://mega.nz කෑල්ල අයින් කරලා කේතය විතරක් ගන්නවා
-                            const megaCode = url.replace('https://mega.nz', '');
-                            
-                            // 🧚‍♂️ ඔයා ඉල්ලපු විදිහටම DILSHAN-MD~ කෑල්ල ඉස්සරහට එකතු කරනවා
-                            const shortSessionId = `DILSHAN-MD~${megaCode}`;
-                            
-                            resolve(shortSessionId);
+                            resolve(url);
                         }
                     });
                 });
@@ -66,13 +59,7 @@ export const upload = (filePath, fileName) => {
 export const download = (url) => {
     return new Promise((resolve, reject) => {
         try {
-            // සෙෂන් ID එකෙන් නැවත ඩවුන්ලෝඩ් කරද්දී DILSHAN-MD~ කෑල්ල තිබ්බොත් ඒක අයින් කරලා මුල් ලින්ක් එක හදනවා
-            let rawUrl = url;
-            if (url.includes('DILSHAN-MD~')) {
-                rawUrl = 'https://mega.nz' + url.replace('DILSHAN-MD~', '');
-            }
-
-            const file = mega.File.fromURL(rawUrl);
+            const file = mega.File.fromURL(url);
 
             file.loadAttributes((err) => {
                 if (err) {
