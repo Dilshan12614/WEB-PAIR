a8539e28ac07a9fcd5dedd4a1ecf70055712a9c1
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-12 21:57:08 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -4,7 +4,7 @@ import fs from "fs";
 // Mega authentication credentials
 const auth = {
     email: "dilshanashinsa793@gmail.com", // your mega account login email
-    password: "P3i2pc:we6JmpP_", // your mega account login password
+    password: "Rathnayake12", // your mega account login password
     userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
